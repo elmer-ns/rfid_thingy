b3396b6963ebf3ccfd9d081b63caf86d84e38b39
Elmer Nolgren Saldert <174507028+elmer-ns@users.noreply.github.com>
2026-03-11 13:23:45 +0100

    can detect new cards

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,7 +10,8 @@
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_hal_bus::spi::{self, DeviceError, ExclusiveDevice};
+use embedded_io::Read;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
@@ -18,7 +19,7 @@ use esp_hal::gpio::{Level, Output, OutputConfig, OutputPin, Pin};
 use esp_hal::peripherals::GPIO;
 use esp_hal::spi::master::{Config, Spi};
 use esp_hal::timer::timg::TimerGroup;
-use log::info;
+use log::{info, warn};
 use mfrc522::Mfrc522;
 use mfrc522::comm::blocking::i2c::I2cInterface;
 use mfrc522::comm::blocking::spi::SpiInterface;
@@ -50,23 +51,33 @@ async fn main(spawner: Spawner) -> ! {
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
 
-    //let itf = I2cInterface::new(i2c, 0x28);
-
-    let spi: Spi<'_, esp_hal::Blocking> = Spi::new(peripherals.SPI2, Config::default()).unwrap();
+    let spi: Spi<'_, esp_hal::Blocking> = Spi::new(peripherals.SPI2, Config::default()).unwrap().with_sck(peripherals.GPIO9).with_mosi(peripherals.GPIO11).with_miso(peripherals.GPIO12);
     
     let cs_pin = Output::new(peripherals.GPIO10, Level::Low, OutputConfig::default());
 
     let device = ExclusiveDevice::new(spi, cs_pin, Delay::new()).unwrap();
     let itf = SpiInterface::new(device);
 
-    let mut mfrc522 = Mfrc522::new(itf).init().unwrap();
+    let mut mfrc522: Mfrc522<SpiInterface<ExclusiveDevice<Spi<'_, esp_hal::Blocking>, Output<'_>, Delay>, mfrc522::comm::blocking::spi::DummyDelay>, mfrc522::Initialized> = Mfrc522::new(itf).init().unwrap();
 
     let mfrc522_version = mfrc522.version().unwrap();
 
     info!("mfrc522_version={}", mfrc522_version);
 
     loop {
-        info!("Hello world!");
+        if new_card_present(&mut mfrc522) {
+            info!("new card!");
+        }
+
         Timer::after(Duration::from_secs(1)).await;
+    }
+}
+
+type Reader<'a> = Mfrc522<SpiInterface<ExclusiveDevice<Spi<'a, esp_hal::Blocking>, Output<'a>, Delay>, mfrc522::comm::blocking::spi::DummyDelay>, mfrc522::Initialized>;
+
+fn new_card_present(reader: &mut Reader) -> bool {
+    match reader.new_card_present() {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
