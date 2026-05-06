54ef4b31c18e40d5330bea2e7bdd118343cdbbdc
Elmer Nolgren Saldert <174507028+elmer-ns@users.noreply.github.com>
2026-05-06 11:32:57 +0200

    html

--- src/website/js/main.js
+++ src/website/js/main.js
@@ -1,9 +1,12 @@
-var inputs = document.getElementsByClassName('hex_code');
+var inputs = document.querySelectorAll('.hex_code');
 
-for (let i=0; i<inputs.length; i++) {
+for (let i = 0; inputs[i]; i++) {
     let input = inputs[i];
     console.log(input)
     input.addEventListener('input', (event) => {
         input.value = input.value.replace(/[^0-9a-fA-F]+$/, '');
+        input.style.height = "";
+        console.log(input.scrollHeight);
+        input.style.height = input.scrollHeight + "px";
     })
 }
